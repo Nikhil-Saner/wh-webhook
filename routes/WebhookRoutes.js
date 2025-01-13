57d89111439863b423216455f54b699b57e7379b
Nikhil Saner <saner.nikhil@gmail.com>
2025-01-13 18:12:03 +0530

    removed verify

--- routes/WebhookRoutes.js
+++ routes/WebhookRoutes.js
@@ -7,7 +7,7 @@ module.exports = function(webhookController) {
 
   // Define routes and associate with controller methods
   router.get('/abc/test', (req, res) => webhookController.testing(req, res));
-  router.get('/verify', (req, res) => webhookController.verifyWebhook(req, res));
+  router.get('/', (req, res) => webhookController.verifyWebhook(req, res));
   router.post('/incoming', (req, res) => webhookController.handleIncomingMessages(req, res));
   router.get('/message-status/verify', (req, res) => webhookController.verifyMessageStatusWebhook(req, res));
   router.post('/message-status', (req, res) => webhookController.handleMessageStatus(req, res));
